a432675f1c4546f87eaa658fd33af16a5b86bdd6
Jordan <jordanjames1095@googlemail.com>
2020-10-22 20:29:43 +0100

    added shooter

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //draw the alien invaders
 
-    alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+    alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+
+    //draw the shooter
+    squares[currentShooterIndex].classList.add('shooter')
 
 })
